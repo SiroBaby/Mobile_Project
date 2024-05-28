1797df198633152d9947dbd290e60b83feca0813
duco9t <dung5715@gmail.com>
2024-05-28 21:38:05 +0700

    Add layout home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home'
-import Login from './Login';
+// import Login from './Login';
 
 const Stack = createStackNavigator();
 
 const RootLayout = () => {
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+    <Stack.Navigator initialRouteName="Home">
+      {/* <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} /> */}
       <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
