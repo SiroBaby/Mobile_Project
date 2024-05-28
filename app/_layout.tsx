902b1630a2462736e832886f71dd414e9b411b9c
SiroBaby <ngocphat076@gmail.com>
2024-05-28 20:35:56 +0700

    router và chức năng login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router'
-import React from 'react'
-import tw from 'twrnc'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Home'
+import Login from './Login';
 
+const Stack = createStackNavigator();
 
 const RootLayout = () => {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false}} />
-        </Stack>
-    )
-}
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
