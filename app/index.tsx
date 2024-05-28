7cc71201db985e0b2bc1aaa797bb9ed69c835da8
huyen35 <hyhy91177@gmail.com>
2024-05-28 17:35:51 +0700

    css index

--- app/index.tsx
+++ app/index.tsx
@@ -1,102 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
 
-export default function App() {
-  const LoginScreen = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-      // Xử lý logic đăng nhập
-      console.log("Username:", username, "Password:", password);
-    };
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-      <View style={styles.container}>
-        <Image
-          source={require('../assets/images/login.png')}
-          style={styles.logo}
-        />
-        <Text style={[styles.title, { textAlign: 'left' }]}>ĐĂNG NHẬP</Text>
+  const handleLogin = () => {
+    // Xử lý logic đăng nhập
+  };
 
-        <TextInput
-          style={styles.input}
-          placeholder="Tên tài khoản"
-          onChangeText={setUsername}
-          value={username}
-        />
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/login.png')}
+        style={styles.logo}
+      />
+      <Text style={[styles.title, { textAlign: 'left' }]}>ĐĂNG NHẬP</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Mật khẩu"
-          secureTextEntry={true}
-          onChangeText={setPassword}
-          value={password}
-        />
+      <TextInput
+        style={styles.input}
+        placeholder="Tên tài khoản"
+        onChangeText={setUsername}
+        value={username}
+      />
 
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-          <Text style={styles.buttonText}>ĐĂNG NHẬP</Text>
-        </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder="Mật khẩu"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
 
-        <Text style={styles.infoText}>Quản trị viên chưa có tài khoản</Text>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>ĐĂNG NHẬP</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity style={styles.registerButton} onPress={() => {}}>
-          <Text style={[styles.buttonText, { color: '#2E328C' }]}>Đăng kí</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+      <Text style={styles.infoText}>Quản trị viên chưa có tài khoản</Text>
 
-  return <LoginScreen />;
-}
+      <TouchableOpacity style={styles.registerButton} onPress={() => {}}>
+        <Text style={[styles.buttonText, { color: '#2E328C' }]}>Đăng kí</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
     backgroundColor: '#fff',
+    alignItems: 'center',    
   },
   logo: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
+    width: 314,
+    height: 314,
+    marginBottom: 90, 
+    
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10, 
+    textAlign: 'left',
+    color: '#2E328C',
   },
   input: {
-    width: '80%',
     height: 40,
-    borderColor: '#ccc',
     borderWidth: 1,
-    borderRadius: 5,
+    borderColor: '#ccc',
     paddingHorizontal: 10,
-    marginBottom: 20,
+    marginBottom: 30,
+    borderRadius: 5,
+    width: 314, 
+    
   },
   loginButton: {
     backgroundColor: '#2E328C',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    padding: 10,
+    alignItems: 'center',
     borderRadius: 5,
-    marginBottom: 10,
+    marginBottom: 20,
+    width: 314, 
+  },
+  registerButton: {
+    backgroundColor: '#EEF4FF',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 20,
+    
+    width: 314, 
   },
   buttonText: {
     color: '#fff',
     fontWeight: 'bold',
   },
   infoText: {
-    color: '#333',
-    marginBottom: 10,
-  },
-  registerButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#2E328C',
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
+
+export default LoginScreen;
+
