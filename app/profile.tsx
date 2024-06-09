761443d8bfc3b1acee9386fc22052df027744486
huyen35 <hyhy91177@gmail.com>
2024-06-10 03:10:34 +0700

    sửa profile, tạo addnew

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,79 +1,87 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { Link, router } from 'expo-router';
 import tw from 'twrnc';
 
 const StudentInfo = () => {
+    const Update = () => {
+        // chuyển hướng đến trang cập nhật thông tin sv
+    };
     return (
-      <View style={tw`flex-1 bg-white p-4`}>
-            <View style={tw`flex justify-center pt-2 px-4`}>
-                <View style={tw`flex items-center justify-center mb-4`}>
-                    <Image
-                        source={require('@/assets/images/hi.png')} 
-                        style={tw`w-40 h-40 mb-4 rounded-full`}
-                    />
+        <ScrollView contentContainerStyle={tw`flex-1 items-center bg-white p-3`} style={tw`bg-white`}>
+            <View style={tw`mb-3 items-center`}>
+                <View style={tw`flex justify-center pt-2 px-4 mt-2`}>
+                    <View style={tw`flex items-center justify-center mb-5`}>
+                            <Image
+                                source={require('@/assets/images/hi.png')} 
+                                style={tw`w-40 h-40 mb-2 rounded-full`}
+                            />
+                    </View>
                 </View>
-            </View>
-            <View>
-            <Text style={tw`text-lg font-semibold my-2 text-xl font-semibold text-blue-900`}> 
-            <Image
-              source={require('@/assets/images/icon/pen.png')}
-              style={tw`w-5 h-5 `}
-            />
-            {'\r'}Thông tin cá nhân:</Text>
-            </View>
-            <View style={tw`bg-blue-900/5 p-4 rounded-lg mb-4 border-dotted border-2 border-indigo-900`}>
-                <Text>
-                    - Họ và tên: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                </Text>
-                <Text>
-                    - MSSV: <Text style={tw`font-semibold`}>điền vào đây</Text> 
-                </Text>
-                <Text>
-                    - CCCD: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                </Text>
-                <Text>
-                    - Ngày sinh: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                    </Text>
-                <Text>
-                    - Giới tính: <Text style={tw`font-semibold`}>điền vào đây</Text>  
-                </Text>
-                <Text>
-                    - Dân tộc: <Text style={tw`font-semibold`}>điền vào đây</Text> 
-                </Text>
-                <Text>
-                    - Địa chỉ: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                </Text>
-                <Text>
-                    - Quê quán: <Text style={tw`font-semibold`}>điền vào đây</Text> 
-                </Text>
-                <Text> 
-                    - Email: <Text style={tw`font-semibold`}>điền vào đây</Text> 
-                </Text>
-                <Text>
-                    - Số điện thoại: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                </Text>
-            </View>
-            <View>
-            <Text style={tw`text-lg font-semibold my-2 text-xl font-semibold text-blue-900`}>
-            <Image
-              source={require('@/assets/images/icon/gradua.png')}
-              style={tw`w-5 h-5 `}
-            />
-            {'\r'}Thông tin học vấn:</Text>
-            </View>
-            <View style={tw`bg-blue-900/5 p-4 rounded-lg mb-4 border-dotted border-2 border-indigo-900`}>
-                <Text>
-                    - Lớp học: <Text style={tw`font-semibold`}>điền vào đây</Text> 
-                </Text>
-                <Text>
-                    - Ngành học: <Text style={tw`font-semibold`}>điền vào đây</Text>
-                </Text>
-                <Text>
-                    - Cơ sở: <Text style={tw`font-semibold`}> Học viện Hàng Không Việt Nam </Text> {/*mặc định */}
-                </Text>
-            </View>
-      </View>
+                <View>
+                    <Text style={tw`text-lg font-semibold text-xl font-semibold text-blue-900 mb-2`}> 
+                        <Image
+                        source={require('@/assets/images/icon/pen.png')}
+                        style={tw`w-5 h-5 `}
+                        />
+                        {'\r'}Thông tin sinh viên:</Text>
+                    </View>
+                    <View style={tw`bg-blue-900/5 p-3 rounded-xl mb-4 border-dotted border-2 border-gray-400`}>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Họ và tên 
+                        </Text>
+                        <View style={tw`flex flex-row justify-between w-full w-78 mb-2`}>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                MSSV 
+                            </Text>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                CCCD
+                            </Text>
+                        </View>
+                        
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Ngày sinh
+                        </Text>
+                        <View style={tw`flex flex-row justify-between w-full w-78 mb-2`}>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                Giới tính  
+                            </Text>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                Dân tộc
+                            </Text>
+                        </View>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Địa chỉ
+                        </Text>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Quê quán 
+                        </Text>
+                        <View style={tw`flex flex-row justify-between w-full w-78 mb-2`}>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                Email  
+                            </Text>
+                            <Text style={tw` p-3 rounded-xl border-solid border border-gray-400 w-38`}>
+                                Số điện thoại
+                            </Text>
+                        </View>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Lớp học
+                        </Text>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Ngành học
+                        </Text>
+                        <Text style={tw` p-3 rounded-xl mb-2 border-solid border border-gray-400`}>
+                            Cơ sở
+                        </Text>
+                    </View>
+                    <TouchableOpacity
+                        style={tw`h-10 bg-blue-900 py-2 px-3 mb-5 rounded-xl items-center w-30`}
+                        onPress={Update}
+                        >
+                        <Text style={tw`text-white font-bold pt-0.5`}>CẬP NHẬT</Text>
+                    </TouchableOpacity> 
+            </View>           
+      </ScrollView>
     );
   };
   
