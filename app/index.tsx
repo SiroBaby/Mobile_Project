352c8f010ac93f9433f1b35def932fac76c5534d
SiroBaby <ngocphat076@gmail.com>
2024-06-17 18:02:27 +0700

    merge

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     const handleLogin = () => {
-        fetch('http://10.40.2.249:3000/login', {
+        fetch('http://10.40.2.214:3000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
