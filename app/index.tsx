73defd88411b2f292b2c85d0a9fd579b0a5f300b
huyen35 <hyhy91177@gmail.com>
2024-07-04 02:55:30 +0700

    sửa lại layout đăng nhập

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     const handleLogin = () => {
-        fetch('http://192.168.1.7:3000/login', {
+        fetch('http://192.168.0.110:3000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -40,7 +40,7 @@ const Login = () => {
             />
             <Text style={tw`text-3xl font-bold mb-4  text-[#2E328C]`}>ĐĂNG NHẬP</Text>
             <TextInput
-                style={tw`h-10 border border-gray-300 px-3 mb-7 rounded-xl w-78`}
+                style={tw`h-10 border border-gray-300 px-3 mb-3 rounded-xl w-78`}
                 placeholder="Tên tài khoản"
                 onChangeText={setUsername}
                 value={username}
@@ -54,8 +54,14 @@ const Login = () => {
                 value={password}
             />
 
-            <TouchableOpacity style={tw`bg-[#2E328C] p-3 items-center rounded-xl mb-5 w-78`} onPress={handleLogin}>
+            <TouchableOpacity style={tw`bg-[#2E328C] p-3 items-center rounded-xl mb-3 w-78`} onPress={handleLogin}>
                 <Text style={tw`text-white font-bold`}>ĐĂNG NHẬP</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={tw`  items-center mb-5 w-78`}>
+                <Link href={'/Change-pass'}>
+                    <Text style={tw`text-[#2E328C] font-bold`}>Quên mật khẩu?</Text>
+                </Link>
             </TouchableOpacity>
 
             <Text style={tw`text-center mb-5`}>Quản trị viên chưa có tài khoản</Text>
@@ -63,12 +69,6 @@ const Login = () => {
             <TouchableOpacity style={tw`bg-[#EEF4FF] p-3 items-center rounded-xl mb-5 w-78`}>
                 <Link href={'/Sign-up'}>
                     <Text style={tw`text-[#2E328C] font-bold`}>ĐĂNG KÍ</Text>
-                </Link>
-            </TouchableOpacity>
-            
-            <TouchableOpacity style={tw`bg-[#EEF4FF] p-3 items-center rounded-xl mb-5 w-78`}>
-                <Link href={'/Change-pass'}>
-                    <Text style={tw`text-[#2E328C] font-bold`}>ĐỔI MẬT KHẨU </Text>
                 </Link>
             </TouchableOpacity>
         </View>
